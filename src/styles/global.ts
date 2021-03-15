5a53d7c3a14effe600c35fe6284c8236e6241515
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2021-03-15 10:45:12 -0300

    Aula 6 - Fontes do Google Fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -30,7 +30,13 @@ export const GlobalStyle = createGlobalStyle`
 	body{
 		background: var(--background);
 		-webkit-font-smoothing: antialiased;
-
+	}
+	body,input,button,textarea{
+		font-family: "Poppins", sans-serif;
+		font-weight: 400;
+	}
+	h1,h2,h3,h4,h5,strong{
+		font-weight: 600;
 	}
 	button{
 		cursor: pointer;
